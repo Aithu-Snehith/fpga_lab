6feab324b5abd27bfb04284ffb88d1be0badf285
Aithu-Snehith <ee16btech11041@iith.ac.in>
2019-01-13 15:47:50 +0530

    updated noise_removal

--- noise_removal.py
+++ noise_removal.py
@@ -8,22 +8,22 @@ import matplotlib.pyplot as plt
 input_signal,fs = sf.read('Sound_Noise.wav')
 
 f = open("out_v1.txt",'w')
-ser = serial.Serial('/dev/ttyACM1',9600)
+ser = serial.Serial('/dev/ttyACM0',9600)
 count = 0
 print input_signal[0]
 
 time.sleep(5)
 
-for i in input_signal[0:10000]:
-	time.sleep(0.2)
+for i in input_signal:
+	# time.sleep(0.0001)
 	ser.write(str(i))
 	ser.write(" ")
 	y = ser.readline()
 	print 'Input: ' + str(i)
-	print 'y'
-	#print count
-	#f.write(y)
-	#count = count + 1
+	print y
+	print count
+	f.write(y)
+	count = count + 1
 
 
 f.close()
